ca0d959d91c6f9b192d8477bdedf8acdd5451257
Lorenzo <lorenzo.giannella@stud.unifi.it>
2021-11-18 15:12:10 +0100

    refactor main method so it creates an istance of getMessage

--- java.helloworld/src/java/helloworld/HelloWorld.java
+++ java.helloworld/src/java/helloworld/HelloWorld.java
@@ -3,6 +3,6 @@ package java.helloworld;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println(new HelloMessage().getMessage());
 	}
 }
